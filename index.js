3a74ca491a3efa1d5abf1c215d820fbc8dfc1c6c
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-06-15 21:26:42 +0200

    chore: setup env, mongodb

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const express = require("express");
-const cors = req
+const cors = require("cors");
+const env = require("dotenv");
+env.config();
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
+console.log(PORT);
+
+app.get("/", async (req, res) => {
+    res.send("Vamos Argentina!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening to port: http://localhost:${PORT}`)
+})
