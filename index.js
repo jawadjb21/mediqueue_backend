b10f9e9d780afcc192c2c364411bda8c403bb514
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-06-19 05:00:20 +0200

    auth: JWT

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const env = require("dotenv");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 env.config();
 
 const app = express();
@@ -28,6 +29,42 @@ async function run() {
         const db = client.db("mediqueue");
         const tutors = db.collection("tutors");
         const bookings = db.collection("bookings");
+
+        const verifyToken = async (req, res, next) => {
+            const authHeader = req.headers.authorization;
+
+            if (!authHeader) {
+                return res.status(401).json({
+                    "message": "Unauthorised."
+                })
+            };
+
+            const token = authHeader.split(" ")[1];
+
+            if (!token) {
+                return res.status(401).json({
+                    "message": "Unauthorised."
+                })
+            };
+            console.log(token);
+
+            try {
+                const JWKS = createRemoteJWKSet(
+                    new URL(`${process.env.FRONTEND_URL}/api/auth/jwks`)
+                )
+
+                const { payload } = await jwtVerify(token, JWKS, {
+                    issuer: process.env.FRONTEND_URL,
+                    audience: process.env.FRONTEND_URL,
+                })
+                if (payload) {
+                    next();
+                }
+            } catch (error) {
+                console.error('Token validation error:', error);
+                return res.status(403).json({ "message": "Forbidden" });
+            }
+        }
 
         /** 
          * Find relevant tutor.
@@ -58,7 +95,7 @@ async function run() {
         /**
          * POST add-tutors route
         */
-        app.post("/add-tutors", async (req, res) => {
+        app.post("/add-tutors", verifyToken, async (req, res) => {
             const newTutor = req.body;
 
             const result = await tutors.insertOne(newTutor);
@@ -69,7 +106,7 @@ async function run() {
         /**
          * GET dynamic tutor route
         */
-        app.get(`/tutors/:id`, async (req, res) => {
+        app.get(`/tutors/:id`, verifyToken, async (req, res) => {
             const result = await findTutor(req.params.id);
             res.send(result);
         });
@@ -77,7 +114,7 @@ async function run() {
         /**
          *  PATCH tutor route
         */
-        app.patch("/tutors/:id", async (req, res) => {
+        app.patch("/tutors/:id", verifyToken, async (req, res) => {
             const query = {
                 _id: new ObjectId(req.params.id)
             };
@@ -130,7 +167,7 @@ async function run() {
         /**
          * DELETE tutors route
          */
-        app.delete("/tutors/:id", async (req, res) => {
+        app.delete("/tutors/:id", verifyToken, async (req, res) => {
             const query = {
                 _id: new ObjectId(req.params.id),
             };
@@ -143,7 +180,7 @@ async function run() {
         /**
          *  GET my-tutors route
         */
-        app.get("/myTutors/:userId", async (req, res) => {
+        app.get("/myTutors/:userId", verifyToken, async (req, res) => {
             const query = {
                 userId: req.params.userId
             };
@@ -156,7 +193,7 @@ async function run() {
         /**
          * GET bookings route.
          */
-        app.get("/bookings/:id", async (req, res) => {
+        app.get("/bookings/:id", verifyToken, async (req, res) => {
             const query = { userId: req.params.id };
 
             const result = await bookings.find(query).toArray();
@@ -167,7 +204,7 @@ async function run() {
         /**
          * POST bookings route.
         */
-        app.post("/bookings", async (req, res) => {
+        app.post("/bookings", verifyToken, async (req, res) => {
             const newBooking = req.body;
 
             const result = await bookings.insertOne(newBooking);
@@ -178,7 +215,11 @@ async function run() {
         /**
          * DELETE bookings route
          */
-        app.delete("/bookings/:id", async (req, res) => {
+        app.delete("/bookings/:id", verifyToken, async (req, res) => {
+            const headers = req.headers.authorization;
+
+            console.log(headers);
+
             const query = { _id: new ObjectId(req.params.id) };
 
             const result = await bookings.deleteOne(query);
